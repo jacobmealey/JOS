a815de41c34948f0e6ffd841502cec1ea8c92dea
Jacob Mealey <jacobmealey.maine@gmail.com>
2018-02-11 13:59:17 -0500

    cleaned up shell

--- kernel/shell.c
+++ kernel/shell.c
@@ -1,36 +1,42 @@
 #include "shell.h"
 char *command_buffer = "\0";
 
+void clear_buffer()
+{
+		command_buffer = command_buffer - string_length(command_buffer);
+}
 void sh_init()
 {
-	printf("JOS v0.0.1\n", 0x0F);
-	putchar('>', 0x0F);
+	printf("JOS v0.0.1\n", normal);
+	putchar('>', normal);
 }
-void commands_check(char * command)
+int commands_check(char * command)
 {
 	if(string_compare(command, "clear")){
-		clear_screen(0x0F);
+		clear_screen(normal);
+		return 1;
 	}
 	if(string_compare(command, "mkfile")){
 		if(isPartitionFAT32(0 , 0) != 1 ){
-			printf("No fat32 formatted disk!\nplease insert disk on port 0", 0x04);
+			printf("No fat32 formatted disk!\nplease insert disk on port 0", red);
 		}
+		return 1;
 	}
 	if(string_compare(command, "list")){
 		listAllFiles(0);
+		return 1;
 	}
+	return 0;
 }
 void sh_write(char data)
 {
-	putchar(data, 0x0F);
-	if(data == '\n')
+	putchar(data, normal);
+	if(data == '\n' )
 	{
-		printf(command_buffer, 0x0F);
-		printf("\n", 0x0F);
 		commands_check(command_buffer);
-		command_buffer = command_buffer - string_length(command_buffer);
-		putchar('\n', 0x0F);
-		putchar('>', 0x0F);
+		clear_buffer();
+		putchar('\n', normal);
+		putchar('>', normal);
 	}
 	else
 	{
