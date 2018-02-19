cd9af8615f5377c1f2caa11895ce27254d7eeb8f
Jacob Mealey <jacobmealey.maine@gmail.com>
2018-02-18 23:22:07 -0500

    renamed myos.bin to JOS.bin, deleted swap files

--- build_run.sh
+++ build_run.sh
@@ -17,11 +17,11 @@ i386-elf-gcc -c kernel/ata.c -o kernel/ata.o -std=gnu99 -ffreestanding -O2 -Wall
 echo "compiling fat"
 i386-elf-gcc -c kernel/fat.c -o kernel/fat.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 echo "Linking Kernel and Bootloader"
-i386-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib\
+i386-elf-gcc -T linker.ld -o JOS.bin -ffreestanding -O2 -nostdlib\
        	boot.o kernel/keyboard.o kernel/common.o kernel/shell.o kernel/kernel.o kernel/vga.o\
 	kernel/ata.o kernel/fat.o -lgcc
 echo "cleaning up"
 rm *.o
 rm kernel/*.o
 echo "running system"
-qemu-system-x86_64 -kernel myos.bin -drive format=raw,file=disk.img 
+qemu-system-x86_64 -kernel JOS.bin -drive format=raw,file=disk.img 
