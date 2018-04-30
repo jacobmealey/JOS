8d673f2b41057a3b68e54e549c6f384e9602f42f
Jacob Mealey <jacobmealey.maine@gmail.com>
2018-04-30 12:37:17 -0400

    replaced prep_commit with gitignore

--- prep_commit.sh
+++
@@ -1,9 +0,0 @@
-echo "cleaning swap files in root"
-rm .*.s*
-echo "cleaning swap files in kernel folder"
-rm kernel/.*.s*
-echo "removing Mac OS garbage"
-rm .DS_Store
-echo "finished"
-
-git status
